d6744a08d2cfab263b8f7eceb3a5635812c3c6bc
Romanov Alex <alexey.romanov@vrbank.ru>
2021-05-25 18:43:27 +0300

    stop and rerun services

--- scripts/start-all.sh
+++ scripts/start-all.sh
@@ -4,8 +4,8 @@ cd ..
 (
   trap 'kill 0' SIGINT;
   export SPRING_PROFILES_ACTIVE=docker &
-  java -jar warranty-service/build/libs/warranty-service.jar &
-  java -jar warehouse-service/build/libs/warehouse-service.jar &
-  java -jar order-service/build/libs/order-service.jar &
-  java -jar store-service/build/libs/store-service.jar
+  java -jar -Dspring.profiles.active=docker warranty-service/build/libs/warranty-service.jar &
+  java -jar -Dspring.profiles.active=docker warehouse-service/build/libs/warehouse-service.jar &
+  java -jar -Dspring.profiles.active=docker order-service/build/libs/order-service.jar &
+  java -jar -Dspring.profiles.active=docker store-service/build/libs/store-service.jar
 )
