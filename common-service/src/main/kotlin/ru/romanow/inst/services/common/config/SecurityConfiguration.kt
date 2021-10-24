90dff19c02bac22731bdf7b73467d1018c8e8d88
aromanov <aromanov@inno.tech>
2021-10-24 12:32:39 +0300

    dpeloy

--- common-service/src/main/kotlin/ru/romanow/inst/services/common/config/SecurityConfiguration.kt
+++ common-service/src/main/kotlin/ru/romanow/inst/services/common/config/SecurityConfiguration.kt
@@ -1,6 +1,7 @@
 package ru.romanow.inst.services.common.config
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest
+import org.springframework.boot.actuate.health.HealthEndpoint
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -9,6 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+import org.springframework.security.web.util.matcher.RequestMatcher
 import ru.romanow.inst.services.common.properties.ActuatorSecurityProperties
 
 @Configuration
@@ -23,7 +26,7 @@ class SecurityConfiguration(
 
     override fun configure(http: HttpSecurity) {
         // @formatter:off
-        http.requestMatcher(EndpointRequest.toAnyEndpoint())
+        http.requestMatcher(EndpointRequest.toAnyEndpoint().excluding(HealthEndpoint::class.java))
             .authorizeRequests()
             .anyRequest()
             .hasRole(actuatorSecurityProperties.role)
