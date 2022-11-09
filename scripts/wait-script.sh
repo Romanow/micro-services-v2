e10780ab66234e52efb2dad923367b06d20d8f83
aromanov <aromanov@inno.tech>
2022-11-09 13:27:30 +0400

    update version

--- scripts/wait-script.sh
+++ scripts/wait-script.sh
@@ -5,7 +5,7 @@ path=$(dirname "$0")
 
 PIDs=()
 for port in "${PORTS[@]}"; do
-  "$path"/wait-for.sh -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
+  "$path"/wait-for.sh -q -t 120 "http://localhost:$port/manage/health" -- echo "Host localhost:$port is active" &
   PIDs+=($!)
 done
 
