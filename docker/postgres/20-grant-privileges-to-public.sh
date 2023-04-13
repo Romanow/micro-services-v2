dacd3e35591d5e9d84cb07ab719ccc43006beb8f
romanow <romanowalex@mail.ru>
2023-04-13 19:28:12 +0400

    fix tests

--- docker/postgres/20-grant-privileges-to-public.sh
+++ docker/postgres/20-grant-privileges-to-public.sh
@@ -3,7 +3,7 @@
 set -e
 
 export PGPASSWORD=postgres
-psql -U postgres -d store -с 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
-psql -U postgres -d orders -с 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
-psql -U postgres -d warehouse -с 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
-psql -U postgres -d warranty -с 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
+psql -U postgres -d store -c 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
+psql -U postgres -d orders -c 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
+psql -U postgres -d warehouse -c 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
+psql -U postgres -d warranty -c 'GRANT ALL PRIVILEGES ON SCHEMA public TO program;'
